d2d51a2cc75acf3d77969de9081ab0041981f1c2
LuckyShmucky <christianluciano567@gmail.com>
2022-06-27 09:12:10 -0700

    first push

---
+++ petfax/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+
+
+def create_app(): 
+    app = Flask(__name__)
+
+    from . import pet
+    app.register_blueprint(pet.bp)
+    @app.route('/')
+    def hello(): 
+        return 'Hello, PetFax!'
+    @app.route('/pets')
+    def pet_list():
+        return 'Here is a list of pets'
+
+    return app
