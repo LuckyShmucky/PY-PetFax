106908146a6c9449c021eff75cb1fa4873076944
LuckyShmucky <christianluciano567@gmail.com>
2022-06-27 21:24:48 -0700

    features/ add we can add new facts to db and view them in facts page

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask 
-
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
     # app.run(debug=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Wishingwell999!@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,10 +1,16 @@
-from flask import (Blueprint, render_template, request, redirect)
+from flask import (Blueprint, flash, render_template, request, redirect)
+from . import models
 fact_bp = Blueprint('fact', __name__, url_prefix='/facts')
 
-@fact_bp.route('/', methods=['POST', 'GET'])
+@fact_bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+        # print(request.form, flush=True)
         return redirect('/facts')
-    
-    return render_template('fact/index.html')
+    results = models.Fact.query.all()
+    return render_template('fact/index.html', facts=results)
